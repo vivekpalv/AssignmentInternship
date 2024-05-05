91afb6ac7be9ec5c5ac519154736b9ffcf41fa70
Vivek Pal <111427320+vivekpalv@users.noreply.github.com>
2024-05-05 21:44:10 +0530

    Delete assignment directory

--- assignment/src/main/java/managment/assignment/MyUserRepository.java
+++
@@ -1,9 +0,0 @@
-package managment.assignment;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MyUserRepository extends JpaRepository<MyUser,Integer> {
-    MyUser findByEmail(String email);
-}
